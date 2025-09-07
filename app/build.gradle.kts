02a5a1930f270552d638fa55ba40bbd948a56cce
victor <bozhko.v.n@yandex.ru>
2025-09-07 22:41:02 +0300

    add plugin application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     id("java")
+    id("application")
     id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
 
 repositories {
     mavenCentral()
